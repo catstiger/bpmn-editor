894e2f948714437c0e6a059fd124cd7eefad713f
catstiger <catstiger@qq.com>
2023-03-02 08:50:31 +0800

    ID生成

--- src/bpmn/BaseElement.js
+++ src/bpmn/BaseElement.js
@@ -70,10 +70,12 @@ class BaseElement {
     }
 
     static _id() {
+        /*
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
             var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
             return v.toString(16);
-        });
+        });*/
+        return 'node_' + new Date().getTime()
     }
 
     _getElementType() {
